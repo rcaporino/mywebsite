b19fea6ddcf6a67c2ea1ad024fe1ec9a5906b993
Rob Caporino <rcaporino@gmail.com>
2020-02-15 02:55:18 -0500

    test

--- src/views/Test1.vue
+++ src/views/Test1.vue
@@ -3,7 +3,7 @@
       <b-row align-h="center" class="mt-3 m-0" cols="1" cols-md="2" align-content="center">
       <b-col xs="auto" sm="auto" md="auto" lg="8">
           <h1 class="text-center">Test Route 1</h1>
-          <p class="text-center">This is route 1 test</p>
+          <p class="text-center">This is route 1 test Test update</p>
       </b-col>
     </b-row>
   </div>
